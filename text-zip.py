4d9293df2f9f43c944a38bd468bae34d4a7dbfc6
Max Chen <immaxchen@gmail.com>
2018-10-09 01:08:06 +0800

    Update text-zip.py

--- text-zip.py
+++ text-zip.py
@@ -1,20 +1,45 @@
 import zlib
 import base64
+import argparse
 
-text = open('example.txt','rb').read()
+def compress(filename, verbose=False):
+    file = open(filename,'rb').read()
+    data = zlib.compress(file, zlib.Z_BEST_COMPRESSION)
+    text = base64.b64encode(data)
 
-data = zlib.compress(text, zlib.Z_BEST_COMPRESSION)
-tzip = base64.b64encode(data)
+    f = open('compressed.txt','wb')
+    f.write(text)
+    f.close()
 
-f = open('compressed.txt','wb')
-f.write(tzip)
-f.close()
+    if verbose:
+        print('Compression ratio: {0:0.2f}'.format(len(text)/len(file)))
 
-data = base64.b64decode(tzip)
-text = zlib.decompress(data)
+def extract(filename, verbose=False):
+    text = open(filename,'rb').read()
+    data = base64.b64decode(text)
+    file = zlib.decompress(data)
 
-f = open('decompressed.txt','wb')
-f.write(text)
-f.close()
+    f = open('decompressed.dat','wb')
+    f.write(file)
+    f.close()
 
-print('Compression Ratio: {0:0.2f}'.format(len(tzip)/len(text)))
+    if verbose:
+        print('Compression ratio: {0:0.2f}'.format(len(text)/len(file)))
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='filename to compress or extract')
+    parser.add_argument('-c', '--compress', help='compress the file', action='store_true')
+    parser.add_argument('-x', '--extract', help='extract the file', action='store_true')
+    parser.add_argument('-v', '--verbose', help='display information', action='store_true')
+    args = parser.parse_args()
+
+    if args.compress & args.extract:
+        parser.error('--compress and --extract can not be given at the same time')
+
+    if args.compress:
+        compress(args.filename, args.verbose)
+
+    if args.extract:
+        extract(args.filename, args.verbose)
